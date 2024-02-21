470328e15deebc8efcf5b980f9863b542426176a
Atash <atash.hauschild@web.de>
2024-02-21 22:07:41 +0100

    use subprocess.call

--- randompause.py
+++ randompause.py
@@ -1,9 +1,12 @@
-import os
+import subprocess
 import time
 from random import randint
+import os
+
+sound_file_path = os.path.abspath('randompause_muffled_sound.mp3')
 
 while True:
-  work_time = randint(180, 240)
-  time.sleep(work_time)
-  os.system(f"afplay {os.path.abspath('randompause_muffled_sound.mp3')}")
+    work_time = randint(180, 240)
+    time.sleep(work_time)
 
+    subprocess.call(['afplay', sound_file_path])
